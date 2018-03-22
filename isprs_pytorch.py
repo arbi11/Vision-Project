78ff96f775ef5a4f718e42f66bf63e1c2022721f
arbi11 <arbaaz.4050@gmail.com>
2018-03-22 01:02:04 -0400

    pytorch pre training

---
+++ isprs_pytorch.py
@@ -0,0 +1,348 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 00:49:27 2018
+
+@author: arbaaz
+"""
+
+import numpy as np
+from skimage import io
+from glob import glob
+#from tqdm import tqdm_notebook as tqdm
+from sklearn.metrics import confusion_matrix
+import random
+import itertools
+# Matplotlib
+import matplotlib.pyplot as plt
+# Torch imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+import torch.optim.lr_scheduler
+import torch.nn.init
+from torch.autograd import Variable
+
+WINDOW_SIZE = (256, 256) # Patch size
+STRIDE = 32 # Stride for testing
+IN_CHANNELS = 3 # Number of input channels (e.g. RGB)
+FOLDER = "./ISPRS_dataset/" # Replace with your "/path/to/the/ISPRS/dataset/folder/"
+BATCH_SIZE = 10 # Number of samples in a mini-batch
+
+LABELS = ["roads", "buildings", "low veg.", "trees", "cars", "clutter"] # Label names
+N_CLASSES = len(LABELS) # Number of classes
+WEIGHTS = torch.ones(N_CLASSES) # Weights for class balancing
+CACHE = True # Store the dataset in-memory
+
+
+class SegNet(nn.Module):
+    # SegNet network
+    @staticmethod
+    def weight_init(m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.kaiming_normal(m.weight.data)
+    
+    def __init__(self, in_channels=IN_CHANNELS, out_channels=N_CLASSES):
+        super(SegNet, self).__init__()
+        self.pool = nn.MaxPool2d(2, return_indices=True)
+        self.unpool = nn.MaxUnpool2d(2)
+        
+        self.conv1_1 = nn.Conv2d(in_channels, 64, 3, padding=1)
+        self.conv1_1_bn = nn.BatchNorm2d(64)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv1_2_bn = nn.BatchNorm2d(64)
+        
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv2_1_bn = nn.BatchNorm2d(128)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_2_bn = nn.BatchNorm2d(128)
+        
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv3_1_bn = nn.BatchNorm2d(256)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2_bn = nn.BatchNorm2d(256)
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3_bn = nn.BatchNorm2d(256)
+        
+        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv4_1_bn = nn.BatchNorm2d(512)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_2_bn = nn.BatchNorm2d(512)
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3_bn = nn.BatchNorm2d(512)
+        
+        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_1_bn = nn.BatchNorm2d(512)
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2_bn = nn.BatchNorm2d(512)
+        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_3_bn = nn.BatchNorm2d(512)
+        
+        self.conv5_3_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_3_D_bn = nn.BatchNorm2d(512)
+        self.conv5_2_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_2_D_bn = nn.BatchNorm2d(512)
+        self.conv5_1_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv5_1_D_bn = nn.BatchNorm2d(512)
+        
+        self.conv4_3_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_3_D_bn = nn.BatchNorm2d(512)
+        self.conv4_2_D = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv4_2_D_bn = nn.BatchNorm2d(512)
+        self.conv4_1_D = nn.Conv2d(512, 256, 3, padding=1)
+        self.conv4_1_D_bn = nn.BatchNorm2d(256)
+        
+        self.conv3_3_D = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_3_D_bn = nn.BatchNorm2d(256)
+        self.conv3_2_D = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv3_2_D_bn = nn.BatchNorm2d(256)
+        self.conv3_1_D = nn.Conv2d(256, 128, 3, padding=1)
+        self.conv3_1_D_bn = nn.BatchNorm2d(128)
+        
+        self.conv2_2_D = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv2_2_D_bn = nn.BatchNorm2d(128)
+        self.conv2_1_D = nn.Conv2d(128, 64, 3, padding=1)
+        self.conv2_1_D_bn = nn.BatchNorm2d(64)
+        
+        self.conv1_2_D = nn.Conv2d(64, 64, 3, padding=1)
+        self.conv1_2_D_bn = nn.BatchNorm2d(64)
+        self.conv1_1_D = nn.Conv2d(64, out_channels, 3, padding=1)
+        
+        self.apply(self.weight_init)
+        
+    def forward(self, x):
+        # Encoder block 1
+        x = self.conv1_1_bn(F.relu(self.conv1_1(x)))
+        x = self.conv1_2_bn(F.relu(self.conv1_2(x)))
+        x, mask1 = self.pool(x)
+        
+        # Encoder block 2
+        x = self.conv2_1_bn(F.relu(self.conv2_1(x)))
+        x = self.conv2_2_bn(F.relu(self.conv2_2(x)))
+        x, mask2 = self.pool(x)
+        
+        # Encoder block 3
+        x = self.conv3_1_bn(F.relu(self.conv3_1(x)))
+        x = self.conv3_2_bn(F.relu(self.conv3_2(x)))
+        x = self.conv3_3_bn(F.relu(self.conv3_3(x)))
+        x, mask3 = self.pool(x)
+        
+        # Encoder block 4
+        x = self.conv4_1_bn(F.relu(self.conv4_1(x)))
+        x = self.conv4_2_bn(F.relu(self.conv4_2(x)))
+        x = self.conv4_3_bn(F.relu(self.conv4_3(x)))
+        x, mask4 = self.pool(x)
+        
+        # Encoder block 5
+        x = self.conv5_1_bn(F.relu(self.conv5_1(x)))
+        x = self.conv5_2_bn(F.relu(self.conv5_2(x)))
+        x = self.conv5_3_bn(F.relu(self.conv5_3(x)))
+        x, mask5 = self.pool(x)
+        
+        # Decoder block 5
+        x = self.unpool(x, mask5)
+        x = self.conv5_3_D_bn(F.relu(self.conv5_3_D(x)))
+        x = self.conv5_2_D_bn(F.relu(self.conv5_2_D(x)))
+        x = self.conv5_1_D_bn(F.relu(self.conv5_1_D(x)))
+        
+        # Decoder block 4
+        x = self.unpool(x, mask4)
+        x = self.conv4_3_D_bn(F.relu(self.conv4_3_D(x)))
+        x = self.conv4_2_D_bn(F.relu(self.conv4_2_D(x)))
+        x = self.conv4_1_D_bn(F.relu(self.conv4_1_D(x)))
+        
+        # Decoder block 3
+        x = self.unpool(x, mask3)
+        x = self.conv3_3_D_bn(F.relu(self.conv3_3_D(x)))
+        x = self.conv3_2_D_bn(F.relu(self.conv3_2_D(x)))
+        x = self.conv3_1_D_bn(F.relu(self.conv3_1_D(x)))
+        
+        # Decoder block 2
+        x = self.unpool(x, mask2)
+        x = self.conv2_2_D_bn(F.relu(self.conv2_2_D(x)))
+        x = self.conv2_1_D_bn(F.relu(self.conv2_1_D(x)))
+        
+        # Decoder block 1
+        x = self.unpool(x, mask1)
+        x = self.conv1_2_D_bn(F.relu(self.conv1_2_D(x)))
+        x = F.log_softmax(self.conv1_1_D(x))
+        return x
+    
+net = SegNet()
+print(net.summary())
+
+import os
+try:
+    from urllib.request import URLopener
+except ImportError:
+    from urllib import URLopener
+
+# Download VGG-16 weights from PyTorch
+vgg_url = 'https://download.pytorch.org/models/vgg16_bn-6c64b313.pth'
+if not os.path.isfile('./vgg16_bn-6c64b313.pth'):
+    weights = URLopener().retrieve(vgg_url, './vgg16_bn-6c64b313.pth')
+
+vgg16_weights = torch.load('./vgg16_bn-6c64b313.pth')
+mapped_weights = {}
+for k_vgg, k_segnet in zip(vgg16_weights.keys(), net.state_dict().keys()):
+    if "features" in k_vgg:
+        mapped_weights[k_segnet] = vgg16_weights[k_vgg]
+        print("Mapping {} to {}".format(k_vgg, k_segnet))
+        
+try:
+    net.load_state_dict(mapped_weights)
+    print("Loaded VGG-16 weights in SegNet !")
+except:
+    # Ignore missing keys
+    pass
+
+base_lr = 0.01
+params_dict = dict(net.named_parameters())
+params = []
+for key, value in params_dict.items():
+    if '_D' in key:
+        # Decoder weights are trained at the nominal learning rate
+        params += [{'params':[value],'lr': base_lr}]
+    else:
+        # Encoder weights are trained at lr / 2 (we have VGG-16 weights as initialization)
+        params += [{'params':[value],'lr': base_lr / 2}]
+
+optimizer = optim.SGD(net.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0005)
+# We define the scheduler
+scheduler = optim.lr_scheduler.MultiStepLR(optimizer, [25, 35, 45], gamma=0.1)
+
+def test(net, test_ids, all=False, stride=WINDOW_SIZE[0], batch_size=BATCH_SIZE, window_size=WINDOW_SIZE):
+    # Use the network on the test set
+    test_images = (1 / 255 * np.asarray(io.imread(DATA_FOLDER.format(id)), dtype='float32') for id in test_ids)
+    test_labels = (np.asarray(io.imread(LABEL_FOLDER.format(id)), dtype='uint8') for id in test_ids)
+    eroded_labels = (convert_from_color(io.imread(ERODED_FOLDER.format(id))) for id in test_ids)
+    all_preds = []
+    all_gts = []
+    
+    # Switch the network to inference mode
+    net.eval()
+
+    for img, gt, gt_e in tqdm(zip(test_images, test_labels, eroded_labels), total=len(test_ids), leave=False):
+        pred = np.zeros(img.shape[:2] + (N_CLASSES,))
+
+        total = count_sliding_window(img, step=stride, window_size=window_size) // batch_size
+        for i, coords in enumerate(tqdm(grouper(batch_size, sliding_window(img, step=stride, window_size=window_size)), total=total, leave=False)):
+            # Display in progress results
+            if i > 0 and total > 10 and i % int(10 * total / 100) == 0:
+                    _pred = np.argmax(pred, axis=-1)
+                    fig = plt.figure()
+                    fig.add_subplot(1,3,1)
+                    plt.imshow(np.asarray(255 * img, dtype='uint8'))
+                    fig.add_subplot(1,3,2)
+                    plt.imshow(convert_to_color(_pred))
+                    fig.add_subplot(1,3,3)
+                    plt.imshow(gt)
+                    clear_output()
+                    plt.show()
+                    
+            # Build the tensor
+            image_patches = [np.copy(img[x:x+w, y:y+h]).transpose((2,0,1)) for x,y,w,h in coords]
+            image_patches = np.asarray(image_patches)
+            image_patches = Variable(torch.from_numpy(image_patches).cuda(), volatile=True)
+            
+            # Do the inference
+            outs = net(image_patches)
+            outs = outs.data.cpu().numpy()
+            
+            # Fill in the results array
+            for out, (x, y, w, h) in zip(outs, coords):
+                out = out.transpose((1,2,0))
+                pred[x:x+w, y:y+h] += out
+            del(outs)
+
+        pred = np.argmax(pred, axis=-1)
+
+        # Display the result
+        clear_output()
+        fig = plt.figure()
+        fig.add_subplot(1,3,1)
+        plt.imshow(np.asarray(255 * img, dtype='uint8'))
+        fig.add_subplot(1,3,2)
+        plt.imshow(convert_to_color(pred))
+        fig.add_subplot(1,3,3)
+        plt.imshow(gt)
+        plt.show()
+
+        all_preds.append(pred)
+        all_gts.append(gt_e)
+
+        clear_output()
+        # Compute some metrics
+        metrics(pred.ravel(), gt_e.ravel())
+        accuracy = metrics(np.concatenate([p.ravel() for p in all_preds]), np.concatenate([p.ravel() for p in all_gts]).ravel())
+    if all:
+        return accuracy, all_preds, all_gts
+    else:
+        return accuracy
+    
+from IPython.display import clear_output
+
+def train(net, optimizer, epochs, scheduler=None, weights=WEIGHTS, save_epoch = 5):
+    losses = np.zeros(1000000)
+    mean_losses = np.zeros(100000000)
+    weights = weights.cuda()
+
+    criterion = nn.NLLLoss2d(weight=weights)
+    iter_ = 0
+    
+    for e in range(1, epochs + 1):
+        if scheduler is not None:
+            scheduler.step()
+        net.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = Variable(data.cuda()), Variable(target.cuda())
+            optimizer.zero_grad()
+            output = net(data)
+            loss = CrossEntropy2d(output, target, weight=weights)
+            loss.backward()
+            optimizer.step()
+            
+            losses[iter_] = loss.data[0]
+            mean_losses[iter_] = np.mean(losses[max(0,iter_-100):iter_])
+            
+            if iter_ % 100 == 0:
+                clear_output()
+                rgb = np.asarray(255 * np.transpose(data.data.cpu().numpy()[0],(1,2,0)), dtype='uint8')
+                pred = np.argmax(output.data.cpu().numpy()[0], axis=0)
+                gt = target.data.cpu().numpy()[0]
+                print('Train (epoch {}/{}) [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccuracy: {}'.format(
+                    e, epochs, batch_idx, len(train_loader),
+                    100. * batch_idx / len(train_loader), loss.data[0], accuracy(pred, gt)))
+                plt.plot(mean_losses[:iter_]) and plt.show()
+                fig = plt.figure()
+                fig.add_subplot(131)
+                plt.imshow(rgb)
+                plt.title('RGB')
+                fig.add_subplot(132)
+                plt.imshow(convert_to_color(gt))
+                plt.title('Ground truth')
+                fig.add_subplot(133)
+                plt.title('Prediction')
+                plt.imshow(convert_to_color(pred))
+                plt.show()
+            iter_ += 1
+            
+            del(data, target, loss)
+            
+        if e % save_epoch == 0:
+            # We validate with the largest possible stride for faster computing
+            acc = test(net, test_ids, all=False, stride=min(WINDOW_SIZE))
+            torch.save(net.state_dict(), './segnet256_epoch{}_{}'.format(e, acc))
+    torch.save(net.state_dict(), './segnet_final')
+
+train(net, optimizer, 50, scheduler)
+net.load_state_dict(torch.load('./segnet_final'))
+
+_, all_preds, all_gts = test(net, test_ids, all=True, stride=32)
+
+for p, id_ in zip(all_preds, test_ids):
+    img = convert_to_color(p)
+    plt.imshow(img) and plt.show()
+    io.imsave('./inference_tile_{}.png'.format(id_), img)
